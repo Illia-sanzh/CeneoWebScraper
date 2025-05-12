d3d7579f6422174f6924a55ecfbc8abc14e1a950
Illia Sanzharovskyi <sanzharovskiy.ilya@gmail.com>
2025-05-12 17:47:18 +0200

    2025-05-12 IS

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/extract')
+def extract():
+    return render_template('extract.html')
+
+@app.route('/product/<product_id>')
+def product(product_id):
+    return render_template('product.html', product_id=product_id)
+
+@app.route('/charts/<product_id>')
+def charts(product_id):
+    return render_template('charts.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
