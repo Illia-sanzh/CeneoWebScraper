50c7ea714e841c07f6586f0937d7d06668d7d4a2
Illia <sanzharovskiy.ilya@gmail.com>
2025-06-09 17:11:22 +0200

    2025-06-09 IS

--- app/models.py
+++ app/models.py
@@ -3,10 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
 import os
-from app.utils import extract_data, translate_data, create_directory_even_if_what_we_call_directory_is_merely_a_binary_code_in_a_soulless_machine_we_humans_do_tend_to_bestow_things_with_meaning_they_do_not_posess_in_the_realm_of_epistemologic_definition
+from app.utils import extract_data, translate_data, make_dir_if_not
 from app.config import headers
+
+matplotlib.use('agg')
+
 class Product:
     def __init__(self, product_id, product_name='', stats={}, opinions=[]):
         self.product_id = product_id
@@ -24,18 +28,6 @@ class Product:
         """ #FFFFFF -> [255,255,255]"""
         #Pass 16 to the integer function for change of base
         return [int(hex_str[i:i+2], 16) for i in range(1,6,2)]
-    
-    def get_color_gradient(self, c1, c2, n):
-        """
-        Given two hex colors, returns a color gradient
-        with n colors.
-        """
-        assert n > 1
-        c1_rgb = np.array(self.hex_to_RGB(c1))/255
-        c2_rgb = np.array(self.hex_to_RGB(c2))/255
-        mix_pcts = [x/(n-1) for x in range(n)]
-        rgb_colors = [((1-mix)*c1_rgb + (mix*c2_rgb)) for mix in mix_pcts]
-        return ["#" + "".join([format(int(round(val*255)), "02x") for val in item]) for item in rgb_colors]
     
     def get_link(self): 
         return f"https://www.ceneo.pl/{self.product_id}#tab-reviews"
@@ -79,46 +71,47 @@ class Product:
         self.stats["average_score"] = opinions.stars.mean()
         self.stats["pros"] = opinions.pros_en.explode().value_counts().to_dict()
         self.stats["cons"] = opinions.cons_en.explode().value_counts().to_dict()
-        self.stats['recommendation']  = opinions.recommend.value_counts(dropna=False).reindex([True, False, np.nan], fill_value=0).to_dict()
+        self.stats['recommendations']  = opinions.recommend.value_counts(dropna=False).reindex([True, False, np.nan], fill_value=0).to_dict()
         self.stats['stars'] = opinions.stars.value_counts().reindex(list(np.arange(0.5,5.5,0.5)), fill_value=0).to_dict()
 
     def generate_charts(self):
-        create_directory_even_if_what_we_call_directory_is_merely_a_binary_code_in_a_soulless_machine_we_humans_do_tend_to_bestow_things_with_meaning_they_do_not_posess_in_the_realm_of_epistemologic_definition('.app/static/piecharts')
-        create_directory_even_if_what_we_call_directory_is_merely_a_binary_code_in_a_soulless_machine_we_humans_do_tend_to_bestow_things_with_meaning_they_do_not_posess_in_the_realm_of_epistemologic_definition('.app/static/barcharts')
-        recommendations = pd.Series(self.stats['recommendations'], index = self.stats['recommendations'].keys())
-        recommendations.plot.plt(
-            label='',
-            colors  = ['forestgreen', 'red', 'blue'],
-            labels=['Recommend', 'Not recommend', 'No opinion']
-        )
-        plt.title(f"Recommendations for product id: {self.product_id}\nNumber of opinions: {self.stats['opinions_count']}")
-        plt.savefig(f".app/static/piecharts/{self.product_id}.png")
+        make_dir_if_not('./app/static/pie_charts')
+        make_dir_if_not('./app/static/bar_charts')
+        recommendations = pd.Series(self.stats['recommendations'], index=self.stats['recommendations'].keys())
+        recommendations.plot.pie(
+            label = "",
+            labels = ["Recommend", "Not recommend", "No opinion"],
+            colors = ['forestgreen', 'crimson', 'steelblue'],
+            autopct = lambda r: f"{r:.1f}%" if r>0 else ""
+            )
+        plt.title(f"Recommendations for product: {self.product_id}\nTotal number of opinions: {self.stats['opinions_count']}")
+        plt.savefig(f"./app/static/pie_charts/{self.product_id}.png")
         plt.close()
 
-        color1='#FF0000'
-        color2='#0000FF'
         plt.figure(figsize=(7,6))
-        ax = self.stats['stars'].plot.bar(color = self.get_color_gradient(color1,color2,15))
+        stars = pd.Series(self.stats['stars'])
+        ax = stars.plot.bar(
+            color = ["forestgreen" if s>3.5 else "crimson" if s<3 else "steelblue" for s in stars.index]
+        )
         plt.bar_label(container=ax.containers[0])
         plt.xlabel("Number of stars")
         plt.ylabel("Number of opinions")
-        plt.title(f"Number of opinions about product:{self.product_id}\n with particular")
+        plt.title(f"Number of opinions about product {self.product_id}\nwith particular number of stars\nTotal number of opinions: {self.stats['opinions_count']}")
         plt.xticks(rotation=0)
-        plt.savefig(f".app/static/barcharts/{self.product_id}.png")
+        plt.savefig(f"./app/static/bar_charts/{self.product_id}.png")
         plt.close()
 
     def save_opinions(self):
-        create_directory_even_if_what_we_call_directory_is_merely_a_binary_code_in_a_soulless_machine_we_humans_do_tend_to_bestow_things_with_meaning_they_do_not_posess_in_the_realm_of_epistemologic_definition('./app/data')
-        create_directory_even_if_what_we_call_directory_is_merely_a_binary_code_in_a_soulless_machine_we_humans_do_tend_to_bestow_things_with_meaning_they_do_not_posess_in_the_realm_of_epistemologic_definition('./app/data/opinions')
+        make_dir_if_not('./app/data')
+        make_dir_if_not('./app/data/opinions')
         with open(f'./app/data/opinions/{self.product_id}.json', 'w', encoding='UTF-8') as f:
-            json.dump(self.opinions_to_dict, f, indent=4, ensure_ascii=False)
+            json.dump(self.opinions_to_dict(), f, indent=4, ensure_ascii=False)
     def save_info(self):
-        create_directory_even_if_what_we_call_directory_is_merely_a_binary_code_in_a_soulless_machine_we_humans_do_tend_to_bestow_things_with_meaning_they_do_not_posess_in_the_realm_of_epistemologic_definition('./app/data')
-        create_directory_even_if_what_we_call_directory_is_merely_a_binary_code_in_a_soulless_machine_we_humans_do_tend_to_bestow_things_with_meaning_they_do_not_posess_in_the_realm_of_epistemologic_definition('./app/data/products')
+        make_dir_if_not('./app/data')
+        make_dir_if_not('./app/data/products')
         with open(f'./app/data/products/{self.product_id}.json', 'w', encoding='UTF-8') as f:
-            json.dump(self.info_to_dict, f, indent=4, ensure_ascii=False)
-        
-
+            json.dump(self.info_to_dict(), f, indent=4, ensure_ascii=False)
+    
 class Opinion:
     selectors = {
         'opinion_id': (None, 'data-entry-id',),
@@ -157,7 +150,7 @@ class Opinion:
     def __repr__(self):
         return "Opinion("+', '.join([f'{key}={getattr(self,key)}'for key in self.selectors.keys()])+')'
     def to_dict(self):
-        return {{key}: getattr(self,key)for key in self.selectors.keys()}
+        return {key: getattr(self,key)for key in self.selectors.keys()}
     
     def extract(self, opinion):
         for key, values in self.selectors.items():
--- app/utils.py
+++ app/utils.py
@@ -23,6 +23,6 @@ def extract_data(ancestor, selector=None, attribute=None, multiple=False):
 def translate_data(text, source='pl', target='en'):
     return GoogleTranslator(source, target).translate(text=text)
 
-def create_directory_even_if_what_we_call_directory_is_merely_a_binary_code_in_a_soulless_machine_we_humans_do_tend_to_bestow_things_with_meaning_they_do_not_posess_in_the_realm_of_epistemologic_definition(dir):
+def make_dir_if_not(dir):
     if not os.path.exists(dir):
         os.mkdir(dir)
